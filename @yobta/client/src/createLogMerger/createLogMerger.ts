560ad3621ac7c5117f10affed605949760a91315
droganov <serge@droganov.ru>
2023-03-29 11:13:48 +0200

    fix insert, delete, insert bug

--- @yobta/client/src/createLogMerger/createLogMerger.ts
+++ @yobta/client/src/createLogMerger/createLogMerger.ts
@@ -25,6 +25,7 @@ const insert = <Snapshot extends YobtaCollectionAnySnapshot>(
   if (!snapshot || snapshot.id === nextSnapshotId) {
     return snapshots
   }
+  // todo: find last index
   const index = nextSnapshotId
     ? snapshots.findIndex(({ id }) => id === nextSnapshotId)
     : -1
@@ -40,14 +41,18 @@ export const createLogMerger: YobtaLogMergerFactory =
     getSnapshot: YobtaGetCollectionSnapshot<Snapshot>,
   ) =>
   entries => {
-    const deleted = new Set<YobtaCollectionId>()
+    const deleted = new Map<YobtaCollectionId, number>()
+    const getCount = (id: YobtaCollectionId): number => deleted.get(id) || 0
+    const setCount = (id: YobtaCollectionId, count: number): void => {
+      deleted.set(id, getCount(id) + count)
+    }
     const mergeResult = entries
       .reduce<YobtaLogEntry[]>((acc, entry) => {
         switch (entry[4]) {
           case YOBTA_REJECT:
             return acc.filter(([id]) => id !== entry[7])
           case YOBTA_COLLECTION_DELETE:
-            deleted.add(entry[5])
+            setCount(entry[5], 1)
             return acc
           case YOBTA_COLLECTION_RESTORE:
             deleted.delete(entry[5])
@@ -60,10 +65,6 @@ export const createLogMerger: YobtaLogMergerFactory =
       .reduce<Snapshot[]>((acc, [, , , , type, snapshotId, nextSnapshotId]) => {
         switch (type) {
           case YOBTA_COLLECTION_INSERT: {
-            if (deleted.has(snapshotId)) {
-              deleted.delete(snapshotId)
-              return acc
-            }
             const snapshot = getSnapshot(snapshotId)
             return insert(acc, snapshot, nextSnapshotId)
           }
@@ -86,5 +87,12 @@ export const createLogMerger: YobtaLogMergerFactory =
           }
         }
       }, [])
+      .filter(snapshot => {
+        if (getCount(snapshot.id) > 0) {
+          setCount(snapshot.id, -1)
+          return false
+        }
+        return true
+      })
     return mergeResult
   }
